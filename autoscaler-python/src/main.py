4b9d1b569cf09ec43b8595e458227fb2a33ed64e
Cristian Malaia <cristian.malaia@gmail.com>
2021-11-13 01:52:01 +0000

    Stuff

--- autoscaler-python/src/main.py
+++ autoscaler-python/src/main.py
@@ -2,89 +2,105 @@ import signal
 import datetime
 import sys
 import os
-from time import sleep
+from threading import Event
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
 
-def print_to_stdout(*a):
-    # Here a is the array holding the objects
-    # passed as the argument of the function
-    print(*a, file=sys.stdout)
+EXIT = Event()
 
-class GracefulKiller:
-    kill_now = False
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+class AutoScaler:
     def __init__(self):
-        signal.signal(signal.SIGINT, self.exit_gracefully)
-        signal.signal(signal.SIGTERM, self.exit_gracefully)
+        config.load_incluster_config()
+#        v1 = client.CoreV1Api()
+#        print("Listing pods with their IPs:")
+#        ret = v1.list_pod_for_all_namespaces(watch=False)
+#        for i in ret.items:
+#            print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        self.k8s = client.AppsV1Api()
+        self.processes_started = 0
+        self.n_replicas = 1
+        self.camunda_url = os.getenv('CAMUNDA_URL',
+                                     "http://camunda-service:8080/engine-rest/history/process-instance/count")
+        print(f"CAMUNDA_URL {self.camunda_url}")
+        self.last_call = datetime.datetime.now() - datetime.timedelta(seconds=10)
 
-    def status(self):
-        print_to_stdout(f"I am in {self.kill_now} status")
-
-    def exit_gracefully(self, *_):
-        self.kill_now = True
-
-def requests_retry_session(
-        retries=3,
-        backoff_factor=0.3,
-        status_forcelist=(500, 502, 504),
-        session=None,
-):
-    session = session or requests.Session()
-    retry = Retry(
-        total=retries,
-        read=retries,
-        connect=retries,
-        backoff_factor=backoff_factor,
-        status_forcelist=status_forcelist,
-    )
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-    return session
+    @staticmethod
+    def requests_retry_session(
+            retries=3,
+            backoff_factor=0.3,
+            status_forcelist=(500, 502, 504),
+            session=None,
+    ):
+        session = session or requests.Session()
+        retry = Retry(
+            total=retries,
+            read=retries,
+            connect=retries,
+            backoff_factor=backoff_factor,
+            status_forcelist=status_forcelist,
+        )
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
+        return session
 
 
-def autoscale():
-    processes_started = 0
-    n_replicas = 1
-    camunda_url = os.getenv('CAMUNDA_URL', "http://camunda-service:8080/engine-rest/history/process-instance/count")
-    print_to_stdout(f"CAMUNDA_URL {camunda_url}")
-    last_call = datetime.datetime.now() - datetime.timedelta(seconds=10)
-    while not KILLER.kill_now:
-        print_to_stdout("every xxx")
+    def scale(self):
         try:
-            response = requests_retry_session().get(
-                camunda_url,
-                params={'timestamp': format_date_camunda(last_call)},
+            response = self.requests_retry_session().get(
+                self.camunda_url,
+                params={'timestamp': format_date_camunda(self.last_call)},
                 headers={'Content-Type': 'application/json'},
             )
         except requests.exceptions.HTTPError as errh:
-            print_to_stdout(f"Http Error: {errh}")
+            eprint(f"Http Error: {errh}")
         except requests.exceptions.ConnectionError as errc:
-            print_to_stdout(f"Error Connecting: {errc}")
+            eprint(f"Error Connecting: {errc}")
         except requests.exceptions.Timeout as errt:
-            print_to_stdout(f"Timeout Error: {errt}")
+            eprint(f"Timeout Error: {errt}")
         except requests.exceptions.RequestException as err:
-            print_to_stdout(f"OOps: Something Else {err}")
+            eprint(f"OOps: Something Else {err}")
         else:
-            print('It eventually worked', response.status_code)
             if response.status_code == 200:
-                print_to_stdout('Success!')
                 json_response = response.json()
-                print_to_stdout(f'Received response: {json_response}')
+                self.processes_started = json_response["count"]
+                processes_started_per_instance = self.processes_started / self.n_replicas
+                duration = datetime.datetime.now()-self.last_call
+                print(f"Camunda Engine Replicas: {self.n_replicas}, "
+                      f"Processes started in the last {duration.total_seconds()} "
+                      f"seconds: {self.processes_started}, processes per instance: {processes_started_per_instance}")
+                if processes_started_per_instance >= 50 and self.n_replicas < 4:
+                    print("Action: Scaling up")
+                    try:
+                        resp = self.k8s.patch_namespaced_replica_set(name="camunda-deployment",
+                                                                     namespace="default",
+                                                                     body={"spec":{"replicas": self.n_replicas + 1}})
+                        print(resp)
+                        self.n_replicas += 1
+                    except ApiException as exc:
+                        print(f"Exception when calling AppsV1Api->patch_namespaced_replica_set: {exc}")
 
-                processes_started_per_instance = processes_started / n_replicas
-                if processes_started_per_instance >= 50 and n_replicas < 4:
-                    print_to_stdout("⬆️ Add 1 replica to Camunda Engine deploymentt")
-                elif processes_started_per_instance <= 20 and n_replicas > 1:
-                    print_to_stdout("⬇️ Remove 1 replica from Camunda Engine deployment")
+                elif processes_started_per_instance <= 20 and self.n_replicas > 1:
+                    print("Action: Scaling down")
+                    try:
+                        resp = self.k8s.patch_namespaced_replica_set(name="camunda-deployment",
+                                                                     namespace="default",
+                                                                     body={"spec":{"replicas": self.n_replicas - 1}})
+                        print(resp)
+                        self.n_replicas -= 1
+                    except ApiException as exc:
+                        print(f"Exception when calling AppsV1Api->patch_namespaced_replica_set: {exc}")
                 else:
-                    print_to_stdout("➖ Do nothing")
-                last_call = datetime.datetime.now()
+                    print("Action: Not scaling")
+                self.last_call = datetime.datetime.now()
             else:
-                print_to_stdout(f'HTTP error occurred: {response.reason} retry in 10')
-        sleep(10)
-    print_to_stdout("End of the program. I was killed gracefully :)")
+                eprint(f'HTTP error occurred: {response.reason} retry in 10')
 
 def format_date_camunda(date_time: datetime.datetime) -> str:
     """
@@ -107,9 +123,19 @@ def format_date_camunda(date_time: datetime.datetime) -> str:
 
 
 def main():
-    autoscale()
+    autoscale = AutoScaler()
+    while not EXIT.is_set():
+        autoscale.scale()
+        EXIT.wait(10)
 
+    print("All done!")
+    # perform any cleanup here
+
+def quit_me(signo, _frame):
+    print("Interrupted by %d, shutting down" % signo)
+    EXIT.set()
 
 if __name__ == '__main__':
-    KILLER = GracefulKiller()
+    for sig in ('TERM', 'HUP', 'INT'):
+        signal.signal(getattr(signal, 'SIG'+sig), quit_me)
     main()
